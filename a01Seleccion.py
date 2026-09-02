39f53546159ddc2969f07f9c25eca7202731e94d
carlosherrerah <carlos.herrera@upa.edu.mx>
2026-09-02 08:58:19 -0600

    Auto 2026-09-02 08:58

--- a01Seleccion.py
+++ a01Seleccion.py
@@ -1,7 +1,28 @@
 import os
 import random
 
+def numero_aleatorio(min, max=100):
+    return random.randint(min, max)
+
+
+def etapa_if(edad):
+    etapa=0
+    if (edad >=1 and edad <=30):
+        etapa = 1
+    if (edad >=31 and edad <=60):
+        etapa =2
+    if (edad >=61 and edad <=90):
+        etapa = 3
+    if (edad >=91):
+        etapa = 4
+    return etapa
+           
+
+
 if __name__ == "__main__":
+    os.system("cls")
     print("Hola Bienvenidos")
+    print(numero_aleatorio(11, 20))
+    print(etapa_if(35))
     print(". . .Hecho")
     
