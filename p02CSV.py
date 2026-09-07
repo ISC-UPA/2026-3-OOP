cfcc52304a3960d29812973510f0455bf139e1e3
carlosherrerah <carlos.herrera@upa.edu.mx>
2026-09-07 08:37:51 -0600

    Auto 2026-09-07 08:37

--- p02CSV.py
+++ p02CSV.py
@@ -5,13 +5,17 @@ import pandas as pd
 from datetime import date
 
 os.system('cls' if os.name == 'nt' else 'clear')
+
 # Leer ubicacion del archivo CSV usando Path   # Recomendado
-# Path(p).resolve() --> Ruta absoluta
+# Path(p).resolve() --> Ruta absoluta y el archivo
 # Path(p).parent()  --> Carpeta padre
 db_file = Path(__file__).resolve().parent /  "Estados.csv"
+print(db_file)
 
-# Leer ubicacion del archivo CSV usando os.path
+# Leer ubicacion del archivo CSV usando os.path  # Clasico o antiguo
 #base_dir = os.path.dirname(os.path.dirname(__file__))  # sube un nivel de donde esta este archivo
 #db_file = os.path.join(base_dir, "2026-3-OOP", "Estados.csv")
+#print(db_file)
+
 
 
