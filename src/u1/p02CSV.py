6e09b7d37b8aeea0a89ba613ab21b492cbd4ef3e
Juan Carlos Herrera <carlos.herrera@upa.edu.mx>
2026-09-11 12:02:27 -0600

    Auto 2026-09-11 12:02

--- src/u1/p02CSV.py
+++ src/u1/p02CSV.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import pandas as pd
 
 sys.path.append(os.getcwd())  # No borrar  
+print("\n",sys.path, "\n")
 
 import src.tools.fn as fn
 #from src.tools import fn
@@ -26,31 +27,32 @@ def consultarPandas(db_file):
 def consultarCSV(db_file):
     nombre = "PAN.csv"
     destino = Path(__file__).resolve().parent.parent.parent / "data" / nombre
+    archivo = open(destino, "w", newline="", encoding="utf-8")
+    escritor = csv.writer(archivo)
+        
+    ciudades = []
+    partidos = {}
 
-    with open(destino, "w", newline="", encoding="utf-8") as archivo:
-        escritor = csv.writer(archivo)
-        ciudades = []
-        partidos = {}
+    with open(db_file, "r", encoding="latin-1", newline="") as lectura:
+        lector_csv = csv.DictReader(lectura)
+        
+        cabeceras = lector_csv.fieldnames
+        print("Cabeceras:", cabeceras)
 
-        with open(db_file, "r", encoding="latin-1", newline="") as lectura:
-            lector_csv = csv.DictReader(lectura)
-            cabeceras = lector_csv.fieldnames
-            print("Cabeceras:", cabeceras)
+        for fila in lector_csv:
+            idCiudad = int(fila["IdEstado"])
+            abr = fila["Abr"]
+            fecha = date.strptime(fila["Inicio"], "%d/%m/%Y")  
 
-            for fila in lector_csv:
-                idCiudad = int(fila["IdEstado"])
-                abr = fila["Abr"]
-                fecha = date.strptime(fila["Inicio"], "%d/%m/%Y")
+            if fila["Partido"] == "PAN":
+                ciudades.append((idCiudad, abr, fecha.year))
+                escritor.writerow([idCiudad, abr, fila["Partido"], fila["Nacimiento"]])
 
-                if fila["Partido"] == "PAN":
-                    ciudades.append((idCiudad, abr, fecha.year))
-                    escritor.writerow([idCiudad, abr, fila["Partido"], fila["Nacimiento"]])
-
-                partidos[fila["Partido"]] = partidos.get(fila["Partido"], 0) + 1
-
+            partidos[fila["Partido"]] = partidos.get(fila["Partido"], 0) + 1
+    archivo.close()
+    
     print(ciudades)
     print("\nConteo de partidos:\n", partidos)
-
 
 def main():
     #db_file = Path(__file__).resolve().parent.parent.parent / "data" / "Estados.csv"
@@ -58,12 +60,12 @@ def main():
     db_file = os.getcwd() + "\\data\\Estados.csv"   # ruta absoluta
     print("Ruta:", db_file)
     
-    consultarPandas(db_file)
-    #consultarCSV(db_file)
+    #consultarPandas(db_file)
+    consultarCSV(db_file)
     print("Número aleatorio:", fn.numero_aleatorio(11, 20))
 
 if __name__ == "__main__":
-    os.system("cls")
+    #os.system("cls")
     main()
     #print("Número aleatorio:", numero_aleatorio(11, 20))
     print("Número aleatorio:", fn.numero_aleatorio(11, 20))
