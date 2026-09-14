a8de8d953fac53cf56b25e0333816781dba32a2a
Juan Carlos Herrera <carlos.herrera@upa.edu.mx>
2026-09-14 14:02:32 -0600

    Auto 2026-09-14 14:02

--- main.py
+++ main.py
@@ -9,17 +9,17 @@ carrera = "TIID"
 from src.u1 import p02CSV as prg
 
 def main():
-    global MATERIA
+    #global MATERIA
     MATERIA ="POO"
     print(f"Bienvenido al curso de {MATERIA} en la carrera de {carrera}")  # POO
    
     #prg.main()
-    runpy.run_module(prg.__name__, run_name="__main__")
-    #runpy.run_path(prg.__file__)
+    #runpy.run_module(prg.__name__, run_name="__main__")
+    runpy.run_path(prg.__file__)
 
 if __name__ == "__main__":
       MATERIA="OOP"  # Variable global
-      os.system('cls' if os.name == 'nt' else 'clear')  # Limpiar pantalla
+      os.system("cls" if os.name == 'nt' else 'clear')  # Limpiar pantalla
       main()
       print(f"Nos vemos mañana  en {MATERIA}") # OOP
       print(". . . Hecho")
--- src/u1/p03Access.py
+++ src/u1/p03Access.py
@@ -4,7 +4,7 @@ import datetime
 import pandas as pd
 import pyodbc
 
-# sys.path.append(os.getcwd())
+sys.path.append(os.getcwd())
 
 from src.tools import fn                             # general
 #from src.tools.fn import numero_aleatorio, sumar     # especifica
@@ -13,7 +13,7 @@ def main():
     numero = fn.numero_aleatorio(90)
     print(f"El número aleatorio es: {numero}")
     
-    db_file = os.getcwd()+ "/src/data/Estados.accdb"
+    db_file = os.getcwd()+ "/data/Estados.accdb"
     conn_str = (
         r"Driver={Microsoft Access Driver (*.mdb, *.accdb)};"
         fr"DBQ={db_file};"
